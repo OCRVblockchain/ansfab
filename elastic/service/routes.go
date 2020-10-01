de84a44029214cc4b635de7becc665dede6f5ff3
stdi0 <denis.kozlov94@icloud.com>
2020-10-01 12:51:41 +0300

    delete redundant logic

--- elastic/service/routes.go
+++
@@ -1,6 +0,0 @@
-package service
-
-func initializeRoutes(s *Webserver) {
-	s.router.GET("/count", s.totalCount)
-	s.router.GET("/rails", s.fetchWheelsets)
-}
--- www/front/src/composables/utils.js
+++
@@ -1,4 +0,0 @@
-import { ref, isRef } from '@vue/composition-api'
-
-export const wrap = v => (isRef(v) ? v : ref(v))
-export const unwrap = v => (isRef(v) ? v.value : v)
